b4587dcfc160b885e99bf65f2cb52d8f91bb2a8f
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2023-12-06 11:12:33 +0530

    Minimum in BST

---
+++ minInBST.cpp
@@ -0,0 +1,9 @@
+int minValue(Node* root) {
+    // Code here
+        if(root == NULL)
+        return -1;
+    if(root->left == NULL)
+        return root->data;
+    
+    minValue(root->left);
+}
